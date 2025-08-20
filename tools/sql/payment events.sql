aa09d6f3f3f704153adebf32eba80224eac61514
Stephen Hill <stephen.trevor.hill@gmail.com>
2025-08-20 11:41:31 -0400

    SWD-3 partially paid status

--- tools/sql/payment events.sql
+++ tools/sql/payment events.sql
@@ -1,3 +1,4 @@
+-- SQLite
 SELECT 
     e.id, 
     e.stripe_event_id, 
@@ -9,11 +10,5 @@ SELECT
 FROM payment_events AS e 
 LEFT JOIN payments AS p 
     ON e.payment_id=p.id
+WHERE p.order_id = 'web0016755'
 ORDER BY received_at DESC;
-
-
-
-INSERT INTO orders 
-    (id, currency, amount_due, status, created_at, updated_at)
-VALUES 
-    ('web0016737', 'usd', 5500, 'AWAITING_PAYMENT', datetime('now'), datetime('now'));
